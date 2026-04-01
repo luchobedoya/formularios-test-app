0c8e2c01f4c257e7d4e600501f7ffd296f3e654b
luchoDev <lucho.bedoya1@gmail.com>
2026-03-31 20:23:18 -0500

    se agrega implementacion de formArray
    se agrega implementacion de formArray

--- src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,12 +1,26 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.html',
   styleUrl: './dynamic-page.scss',
 })
 export default class DynamicPage {
+  private _fb = inject(FormBuilder);
+
+  public myForm = this._fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this._fb.array([
+      ['Metal Gear', Validators.required],
+      ['Final Fantasy', Validators.required],
+    ], Validators.minLength(3))
+  });
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
 
 }
