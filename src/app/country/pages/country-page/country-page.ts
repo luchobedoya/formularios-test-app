ae188077a00831efdf3386a5a38666522e46214b
luchoDev <lucho.bedoya1@gmail.com>
2026-04-19 10:06:12 -0500

    implementacion de signals para manejo de continentes y paises
    implementacion de signals para manejo de continentes y paises

--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -1,8 +1,9 @@
 import { JsonPipe } from '@angular/common';
-import { AfterViewInit, Component, inject, signal } from '@angular/core';
+import { AfterViewInit, Component, effect, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { CountryService } from '../../services/country';
+import { CountryService, Region } from '../../services/country';
 import { Country } from '../../interfaces/country.interface';
+import { count, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -14,7 +15,7 @@ export default class CountryPage implements AfterViewInit {
   fb = inject(FormBuilder);
   countryServices = inject(CountryService);
   regions = signal(this.countryServices.getRegions());
-  countriesByRiegrion = signal<Country[]>([]);
+  countriesByRegion = signal<Country[]>([]);
   bordersByCountry = signal<Country[]>([]);
 
   myForm = this.fb.group(
@@ -31,6 +32,31 @@ export default class CountryPage implements AfterViewInit {
 
    ngAfterViewInit(): void {
     console.log(this.regions());
+  }
+
+  OnFormChanged = effect(( onCleanup ) => {
+    const formRegionChanged = this.OnRegionChanged();
+    onCleanup(() => {
+      formRegionChanged?.unsubscribe();
+      console.log('limpiado');
+    })
+  })
+
+  OnRegionChanged() {
+    return this.myForm.get('region')?.valueChanges
+    .pipe(
+      tap(() => this.myForm.get('country')?.reset('')),
+      tap(() => this.myForm.get('borders')?.reset('')),
+      tap(() => {
+        this.bordersByCountry.set([]);
+        this.countriesByRegion.set([]);
+      }),
+      switchMap((region) => this.countryServices.getCountries(region as Region)),
+      tap((countries) => this.countriesByRegion.set(countries))
+    ).subscribe(countries => {
+        console.log(countries);
+        this.countriesByRegion.set(countries);
+    });
   }
 
 }
